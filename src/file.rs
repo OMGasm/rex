a046022f2c747e497fc3f7733d85ef7d36458889
OMGasm <OMGasm@users.noreply.github.com>
2025-03-27 09:26:54 +1000

    why did i spend so long trying to do buffer window resizing? that was dumb

--- src/file.rs
+++ src/file.rs
@@ -26,22 +26,6 @@ impl FileCursor {
         }
     }
 
-    pub fn set_window(self, window_bytes: usize, window_line_bytes: u64) -> FileCursor {
-        assert_ne!(window_bytes, 0);
-        assert_ne!(window_line_bytes, 0);
-        assert_eq!(
-            window_bytes as u64 % window_line_bytes,
-            0,
-            "Cursor's window_bytes must be divisible by window_line_bytes"
-        );
-        FileCursor {
-            file: BufReader::with_capacity(window_bytes, self.file.into_inner()),
-            window_bytes,
-            window_line_bytes,
-        }
-    }
-}
-impl FileCursor {
     pub fn position(&mut self) -> Result<u64, CursorError> {
         Ok(self.file.stream_position()? / self.window_line_bytes)
     }
@@ -62,6 +46,7 @@ impl FileCursor {
                 file.seek_relative(-(rows * self.window_line_bytes).try_into()?)?;
             }
         }
+        file.fill_buf()?;
 
         Ok(())
     }
@@ -71,6 +56,7 @@ impl FileCursor {
     }
 }
 
+#[derive(Debug)]
 pub enum CursorError {
     InvalidMovement(TryFromIntError),
     IOError(io::Error),
